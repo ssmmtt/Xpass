63e09cc0edc64315b8116163e5eef67ee81e3f91
Jayce <lesiaa@foxmail.com>
2023-11-28 02:57:56 +0800

    主界面开发

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             label1 = new Label();
             textBox1 = new TextBox();
             checkBox1 = new CheckBox();
@@ -36,6 +37,10 @@
             button3 = new Button();
             richTextBox1 = new RichTextBox();
             richTextBox2 = new RichTextBox();
+            notifyIcon1 = new NotifyIcon(components);
+            button4 = new Button();
+            openFileDialog1 = new OpenFileDialog();
+            folderBrowserDialog1 = new FolderBrowserDialog();
             SuspendLayout();
             // 
             // label1
@@ -52,8 +57,9 @@
             textBox1.Location = new Point(74, 148);
             textBox1.Name = "textBox1";
             textBox1.PasswordChar = '*';
+            textBox1.PlaceholderText = "没有设置就不填";
             textBox1.Size = new Size(458, 23);
-            textBox1.TabIndex = 1;
+            textBox1.TabIndex = 5;
             // 
             // checkBox1
             // 
@@ -70,19 +76,21 @@
             // 
             button1.Location = new Point(12, 11);
             button1.Name = "button1";
-            button1.Size = new Size(210, 44);
+            button1.Size = new Size(225, 44);
             button1.TabIndex = 3;
             button1.Text = "选择文件";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += Button1_Click;
             // 
             // button2
             // 
-            button2.Location = new Point(266, 11);
+            button2.Location = new Point(253, 11);
             button2.Name = "button2";
-            button2.Size = new Size(214, 44);
+            button2.Size = new Size(225, 44);
             button2.TabIndex = 4;
             button2.Text = "选择目录";
             button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // button3
             // 
@@ -94,12 +102,14 @@
             button3.TabIndex = 5;
             button3.Text = "解密";
             button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
             // 
             // richTextBox1
             // 
             richTextBox1.Location = new Point(12, 61);
             richTextBox1.Name = "richTextBox1";
-            richTextBox1.Size = new Size(468, 81);
+            richTextBox1.ReadOnly = true;
+            richTextBox1.Size = new Size(466, 81);
             richTextBox1.TabIndex = 6;
             richTextBox1.Text = "";
             // 
@@ -107,15 +117,38 @@
             // 
             richTextBox2.Location = new Point(12, 177);
             richTextBox2.Name = "richTextBox2";
+            richTextBox2.ReadOnly = true;
             richTextBox2.Size = new Size(601, 381);
             richTextBox2.TabIndex = 7;
             richTextBox2.Text = "";
+            // 
+            // notifyIcon1
+            // 
+            notifyIcon1.Text = "notifyIcon1";
+            notifyIcon1.Visible = true;
+            // 
+            // button4
+            // 
+            button4.Enabled = false;
+            button4.Location = new Point(486, 107);
+            button4.Name = "button4";
+            button4.Size = new Size(127, 35);
+            button4.TabIndex = 8;
+            button4.Text = "导出";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.Filter = "会话文件(*.xsh)|*.xsh|所有文件(*.*)|*.*";
+            openFileDialog1.Multiselect = true;
+            openFileDialog1.Title = "选择会话文件";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 17F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(625, 570);
+            Controls.Add(button4);
             Controls.Add(richTextBox2);
             Controls.Add(richTextBox1);
             Controls.Add(button3);
@@ -124,8 +157,11 @@
             Controls.Add(checkBox1);
             Controls.Add(textBox1);
             Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
             Name = "Form1";
-            Text = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Xpass";
             ResumeLayout(false);
             PerformLayout();
         }
@@ -140,5 +176,9 @@
         private Button button3;
         private RichTextBox richTextBox1;
         private RichTextBox richTextBox2;
+        private NotifyIcon notifyIcon1;
+        private Button button4;
+        private OpenFileDialog openFileDialog1;
+        private FolderBrowserDialog folderBrowserDialog1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,73 @@
+using System.Diagnostics;
+using System.Windows.Forms;
+
 namespace Xpass
 {
     public partial class Form1 : Form
     {
+        private string[]? selectedFiles;
+        private string? selectedDir;
+        private string? selectedType;
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            // 如果用户点击了 "确定" 按钮
+            if (openFileDialog1.ShowDialog(this) == DialogResult.OK)
+            {
+                // 获取用户选择的文件路径
+                selectedFiles = openFileDialog1.FileNames;
+
+                if (selectedFiles != null)
+                {
+                    selectedType = "file";
+                    // 清空 RichTextBox
+                    richTextBox1.Clear();
+
+                    // 将数组的每个元素写入 RichTextBox，每个元素占据一行
+                    foreach (string element in selectedFiles)
+                    {
+                        richTextBox1.AppendText(element + Environment.NewLine);
+                    }
+                }
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog(this) == DialogResult.OK)
+            {
+                selectedDir = folderBrowserDialog1.SelectedPath;
+                if (selectedDir != null)
+                {
+                    selectedType = "dir";
+                    // 清空 RichTextBox
+                    richTextBox1.Clear();
+
+                    selectedDir = folderBrowserDialog1.SelectedPath;
+                    richTextBox1.AppendText(selectedDir);
+
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine(selectedType);
+            switch (selectedType)
+            {
+                case "file":
+                    break;
+                case "dir":
+                    break;
+                default:
+                    MessageBox.Show(this, "请选择文件或者目录！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information); ;
+                    break;
+            }
+        }
     }
 }
